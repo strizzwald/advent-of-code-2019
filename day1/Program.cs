d1b74afc1057fa4771246d9af2d0f3d01c16b3e7
Kennedy Sigauke <KSigauke@jhb.dvt.co.za>
2020-02-24 20:05:08 +0200

    COmplete day 1 challenge

---
+++ day1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                string input;
+                int weightSum = 0;
+
+                while ((input = reader.ReadLine()) != null)
+                {
+                    int weight = int.Parse(input);
+
+                    weightSum += (weight / 3) - 2;
+                }
+
+                Console.Write("Fuel requirements: {0}", weightSum);
+                Console.ReadLine();
+            }
+        }
+    }
+}
