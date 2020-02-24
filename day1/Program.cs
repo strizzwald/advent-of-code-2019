2fb8448af1483a198634bad9a0868af3d9c8e3ea
Kennedy Sigauke <KSigauke@jhb.dvt.co.za>
2020-02-24 20:54:03 +0200

    Implement Day 1 part 2

--- day1/Program.cs
+++ day1/Program.cs
@@ -7,7 +7,7 @@ namespace day1
     {
         static void Main(string[] args)
         {
-            using (StreamReader reader = new StreamReader("input.txt"))
+            using (StreamReader reader = new StreamReader("part-1-input.txt"))
             {
                 string input;
                 int weightSum = 0;
@@ -19,7 +19,26 @@ namespace day1
                     weightSum += (weight / 3) - 2;
                 }
 
-                Console.Write("Fuel requirements: {0}", weightSum);
+                Console.WriteLine("Part 1 Fuel requirements: {0}", weightSum);
+            }
+
+            using (StreamReader reader = new StreamReader("part-2-input.txt"))
+            {
+                string input;
+                int totalWeight = 0;
+
+                while ((input = reader.ReadLine()) != null)
+                {
+                    int weight = (int.Parse(input) / 3) - 2;
+
+                    while (weight > 0)
+                    {
+                        totalWeight += weight;
+                        weight = (weight / 3) - 2;
+                    }
+                }
+
+                Console.WriteLine("Part 2 Fuel requirements: {0}", totalWeight);
                 Console.ReadLine();
             }
         }
