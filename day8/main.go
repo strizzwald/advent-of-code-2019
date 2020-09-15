d37f98699de3881e1157e4c0db2ca16dae30084f
Kennedy Sigauke <ksigauke@jhb.dvt.co.za>
2020-09-15 08:18:37 +0200

    Implement day 8

--- day8/main.go
+++ day8/main.go
@@ -6,13 +6,12 @@ import (
 	"log"
 	"math"
 	"strizzwald.github.com/aoc/day8/part1"
+	"strizzwald.github.com/aoc/day8/part2"
 )
 
 const layerSize = 25 * 6
 
 func main() {
-	var layers []part1.Layer
-
 	content, err := ioutil.ReadFile("input.txt")
 
 	if err != nil {
@@ -21,18 +20,21 @@ func main() {
 
 	pixels := string(content)
 	numLayers := len(pixels) / layerSize
-	layers = make([]part1.Layer, numLayers)
-	layerStart := 0
+	
+	// executePart1(pixels, numLayers)
+
+	executePart2(pixels, numLayers)
+}
+
+func executePart1(pixels string, numLayers int) {
+	layers := make([]part1.Layer, numLayers)
+	var layerStart int
 
 	for i := 1; i <= len(layers); i++ {
 		layers[i - 1].AddPixels(string(pixels[layerStart:(layerSize * i)]))
 		layerStart = layerSize * i
 	}
 
-	executePart1(layers)
-}
-
-func executePart1(layers []part1.Layer) {
 	var layer part1.Layer
 	minZeros := math.MaxInt32
 
@@ -44,4 +46,53 @@ func executePart1(layers []part1.Layer) {
 	}
 
 	fmt.Println(layer.NumberOfOnes() * layer.NumberOfTwos())
+}
+
+func executePart2(pixels string, numLayers int) {
+	layers := make([]part2.Layer, numLayers)
+	
+	var layerStart int
+
+	for i := 1; i <= len(layers); i++ {
+		layers[i - 1].AddPixels(string(pixels[layerStart:(layerSize * i)]))
+		layerStart = layerSize * i
+	}
+
+	outputLayer := [6][25]part2.Color{}
+
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 25; j++ {
+			outputLayer[i][j] = part2.Transparent
+		}
+	}
+
+	for pixelY := 0; pixelY < 6; pixelY++ {
+		for pixelX := 0; pixelX < 25; pixelX++ {
+			for _, l := range layers {
+				var layerColor part2.Color = l.GetColor(pixelY, pixelX)
+
+				if (outputLayer[pixelY][pixelX] == part2.Transparent) {
+					outputLayer[pixelY][pixelX] = layerColor
+				}
+			}
+		}
+	}
+
+	result := ""
+
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 25; j++ {
+			if (outputLayer[i][j] == part2.Black) {
+				result += "\u2b1c"
+			}
+
+			if (outputLayer[i][j] == part2.White) {
+				result += "\u2b1b"
+			}
+		}
+
+		result = result + "\n"
+	}
+
+	fmt.Println(result)
 }
--- day8/part2/run.go
+++ day8/part2/run.go
@@ -1,1 +1,45 @@
 package part2
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Color int
+
+const (
+	Black Color = iota
+	White Color = iota
+	Transparent Color = iota
+)
+
+const layerWidth = 25
+const layerHeight = 6
+
+type Layer struct {
+	pixels [6][25]Color
+}
+
+func (l *Layer) AddPixels(pixels string) {
+
+	currentColumn := 0
+
+	for i := 0; i < 6; i++ {
+		for j := i * layerWidth; j < ((i * layerWidth) + 25); j++ {
+			num, err := strconv.Atoi(fmt.Sprintf("%c", pixels[j]))
+
+			l.pixels[i][currentColumn] = Color(num)
+
+			if err != nil {
+				panic(err)
+			}
+
+			currentColumn++
+		}
+		currentColumn = 0
+	}
+}
+
+func (l *Layer) GetColor(i int, j int) Color {
+	return l.pixels[i][j]
+}
